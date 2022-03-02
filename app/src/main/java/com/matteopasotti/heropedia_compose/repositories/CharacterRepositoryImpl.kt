9afc10847f7a5e16460b04b21c81c21e2c321a7c
Matteo Pasotti <matteopasotti1@gmail.com>
2022-03-02 14:55:35 +0000

    added check on images, filtering the ones without image

--- app/src/main/java/com/matteopasotti/heropedia_compose/repositories/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/matteopasotti/heropedia_compose/repositories/CharacterRepositoryImpl.kt
@@ -4,11 +4,14 @@ import com.matteopasotti.heropedia_compose.api.MarvelApi
 import com.matteopasotti.heropedia_compose.models.Character
 
 class CharacterRepositoryImpl(private val marvelApi: MarvelApi): CharacterRepository {
+
+    private val IMGNOTAVAILABLE = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
+
     override suspend fun getCharacters(
         modified: String,
         offset: Int,
         limit: Int
     ): List<Character> {
-        return marvelApi.getCharacters(modified, offset, limit).data.results
+        return marvelApi.getCharacters(modified, offset, limit).data.results.filter { it.thumbnail.path != IMGNOTAVAILABLE}
     }
 }
